f9dcec17a0fedc7c44c533e6621710d96b970397
priyanka897-star <priyankamore8997@gmail.com>
2021-10-05 18:30:46 +0530

    Refactored content of the code

--- Line.java
+++ Line.java
@@ -1,9 +1,13 @@
 package javalinecomparision;
 
 public class Line {
-	Point p1, p2;
+	Point p1, p2,p3,p4;
 	
-    public double getLength(Point p1, Point p2) {
+
+	public double getLengthLine1(Point p1, Point p2) {
 		   return Math.sqrt(Math.pow(p1.x-p2.x,2)+Math.pow(p1.y-p2.y,2));
+	   }
+	public double getLengthLine2(Point p3, Point p4) {
+		   return Math.sqrt(Math.pow(p3.x-p4.x,2)+Math.pow(p3.y-p4.y,2));
 	   }
 }
--- Main.java
+++ Main.java
@@ -16,15 +16,46 @@ public class Main {
 		p2.x = ip.inputInteger();
 		System.out.println("Enter y value for point 2");
 		p2.y = ip.inputInteger();
+		
+		Point p3 = new Point();
+		System.out.println("Enter x value for point 3");
+		p3.x = ip.inputInteger();
+		System.out.println("Enter y value for point 3");
+		p3.y = ip.inputInteger();
+		
+		Point p4= new Point();
+		System.out.println("Enter x value for point 4");
+		p4.x = ip.inputInteger();
+		System.out.println("Enter y value for point 4");
+		p4.y = ip.inputInteger();
 	    ip.scannerClose();
 		
-		Line line = new Line();
-		line.p1 = p1;
-		line.p2 = p2;
+		Line line1 = new Line();
+		line1.p1 = p1;
+		line1.p2 = p2;
 		
-		Double length = line.getLength(p1, p2);
-		System.out.println("Line Length:" +length);
-	}
+		Line line2 = new Line();
+		line2.p3 = p3;
+		line2.p4 = p4;
+		
+		
+		
+		Double lineLength1 = line1.getLengthLine1(p1, p2);
+		Double lineLength2 = line1.getLengthLine2(p3, p4);
+		System.out.println("Line 1 Length:" +lineLength1);
+		System.out.println("Line 2 Length:" +lineLength2);
+	    if (lineLength1.equals(lineLength2))
+             System.out.println("Line1 and Line2 are equal");
+         
+         else 
+       	  System.out.println("Line1 and Line2 are not equal");
+       	  
+    }
+	
 }
+
+
+
+
 
 
