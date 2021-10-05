763df7408b2bb1be301d2cf4ab7ec82f158a5437
priyanka897-star <83417119+priyanka897-star@users.noreply.github.com>
2021-10-05 22:48:39 +0530

    Refactored the content of the code

--- Line.java
+++ Line.java
@@ -2,14 +2,15 @@ package javalinecomparision;
 
 public  class Line {
 	Point p1, p2,p3,p4;
-	final int  var = 0;
 	
 	public double getLengthLine1(Point p1, Point p2) {
 		   return Math.sqrt(Math.pow(p1.x-p2.x,2)+Math.pow(p1.y-p2.y,2));
 	   }
+	
 	public double getLengthLine2(Point p3, Point p4) {
 		   return Math.sqrt(Math.pow(p3.x-p4.x,2)+Math.pow(p3.y-p4.y,2));
 	   }
+	
 	public   void compare(Double lineLength1, Double lineLength2) {
 		
 		// TODO Auto-generated method stub
